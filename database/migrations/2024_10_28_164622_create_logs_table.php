7c8c72aacfcd10c6b37f0e4f269e14140c55d80f
cahbayu <86003665+cahbayu@users.noreply.github.com>
2024-11-12 23:34:34 +0700

    Update 2024_10_28_164622_create_logs_table.php

--- database/migrations/2024_10_28_164622_create_logs_table.php
+++ database/migrations/2024_10_28_164622_create_logs_table.php
@@ -10,18 +10,19 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::create('logs', function (Blueprint $table) {
-            $table->id('log_id'); // Primary Key
-            $table->unsignedBigInteger('node_id'); // Kolom untuk foreign key
-            $table->string('action', 255); // Deskripsi aktivitas
-            $table->string('node_type', 10); // Tambahkan kolom node_type
-            $table->timestamps(); // Kolom created_at dan updated_at
-        
-            // Tentukan foreign key yang mengacu pada node_id
-            $table->foreign('node_id')->references('node_id')->on('nodes')->onDelete('cascade');
-        });            
-    }
+{
+    Schema::create('logs', function (Blueprint $table) {
+        $table->id('log_id'); // Primary Key
+        $table->unsignedBigInteger('node_id'); // Kolom untuk foreign key
+        $table->string('action', 255); // Deskripsi aktivitas
+        $table->string('node_type', 10); // Kolom untuk tipe node
+        $table->integer('data_byte'); // Menyimpan ukuran byte yang dikirim
+        $table->timestamps(); // Kolom created_at dan updated_at
+
+        // Tentukan foreign key yang mengacu pada node_id
+        $table->foreign('node_id')->references('node_id')->on('nodes')->onDelete('cascade');
+    });            
+}
 
     /**
      * Reverse the migrations.
